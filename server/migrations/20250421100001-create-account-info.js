9c22a622beddd797dd67044c708b3e2c226c6014
divv919 <divyanshsoni919@gmail.com>
2025-04-21 20:08:11 +0530

    chore: Add migrations in sequelize ORM to create new Models for account_infos table

---
+++ server/migrations/20250421100001-create-account-info.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("account_infos", {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "user_id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      phone_number: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: true,
+      },
+      first_name: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: true,
+      },
+      last_name: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: true,
+      },
+      address: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("account_infos");
+  },
+};
