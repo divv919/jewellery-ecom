1b41496fc2226161f8a5e281ca3f05e876336efc
divv919 <divyanshsoni919@gmail.com>
2025-04-24 11:30:21 +0530

    fix: debug and fix accountController put route

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,3 +1,41 @@
-export default async (req, res) => {
-  res.status(200).json({ message: "Account Controller works" });
+import models from "../models/index.js";
+
+export const getAccountInfo = async (req, res) => {
+  try {
+    const result = await models.AccountInfo.findAll({
+      where: { user_id: req.user.user_id },
+    });
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const postAccountInfo = async (req, res) => {
+  const newValue = req.body;
+  try {
+    const newInfo = await models.AccountInfo.create({
+      user_id: req.user.user_id,
+      ...newValue,
+    });
+    res.status(200).json(newInfo);
+  } catch (err) {
+    console.error("Couldn't add accountInfo ");
+    res.status(500).json(err);
+  }
+};
+
+export const putAccountInfo = async (req, res) => {
+  const toUpdate = req.body;
+  console.log(req.body);
+  try {
+    const updatedInfo = await models.AccountInfo.update(toUpdate, {
+      where: { user_id: req.user.user_id },
+    });
+    res.status(200).json(updatedInfo);
+  } catch (err) {
+    console.error("Couldn't update accountInfo ");
+    res.status(500).json(err);
+  }
 };
