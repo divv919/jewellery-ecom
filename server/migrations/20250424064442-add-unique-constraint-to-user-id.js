f3e3585b681d04ba2a3f45f565d0ce84e124ce33
divv919 <divyanshsoni919@gmail.com>
2025-04-24 19:04:28 +0530

    fix: quick fixes

---
+++ server/migrations/20250424064442-add-unique-constraint-to-user-id.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint("account_infos", {
+      fields: ["user_id"],
+      type: "unique",
+      name: "unique_user_id",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint("account_infos", "unique_user_id");
+  },
+};
